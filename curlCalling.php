fa5098086c3de556a576fbb5a6077468c766027e
Karthik R <mcakarthikr@gmail.com>
2017-01-29 20:33:33 +0530

    Curl Calling Sample
    Completed Curl Calling Using GET Method for POSTNL server

--- curlCalling.php
+++ curlCalling.php
@@ -1,21 +1,24 @@
 <?php error_reporting( E_ALL & ~E_WARNING); ?>
 <?php
-$ch = curl_init();
-$curlConfig = array(
-    CURLOPT_URL            => "https://www.postnl.nl/locatiewijzer/#/?centerLat=52.473463447473485&centerLng=4.813104818376132&criterium=&picketLat=52.4736696&picketLng=4.812634800000069&productid=&productpanel=mailbox&query=Weefhuispad%201%2C%20zaandijk&searchVisible&zoom=17",
-    CURLOPT_RETURNTRANSFER => true,
-);
-curl_setopt_array($ch, $curlConfig);
-$result = curl_exec($ch);
 echo "<pre>";
-print_r($result );
+print_r(getNearestMail("https://www.postnl.nl/services/adreskenmerken/api/GetLocationsSidebar?criterium=0&product=2&swLat=52.47133944221238&swLng=4.807203958543369&neLat=52.47558735022358&neLng=4.819005678208896&apikey=e9b6ea2efe17510f"));
+echo "</pre>";
 
-$error = curl_error($ch);
-echo "result<br>";
-$dom = new DOMDocument();
-$dom->loadHTML( $result );
-print_r($dom);
-echo "<br>error<br>";
-echo $error;
-curl_close($ch);
+function getNearestMail($url) {
+	$ch = curl_init();
+	$curlConfig = array(
+		CURLOPT_URL            => $url,
+		CURLOPT_RETURNTRANSFER => true,
+	);
+	curl_setopt_array($ch, $curlConfig);
+	$result = curl_exec($ch);
+	$error = curl_error($ch);
+	curl_close($ch);
+	if(!$error) {
+		$object = json_decode($result,true);
+		return $object['list'];
+	} else {
+		return $error;
+	}
+}
 ?>
